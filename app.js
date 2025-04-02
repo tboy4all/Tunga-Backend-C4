55f7647a34e3ca4c8ac0acfebdcac9f5fdac8459
tboy4all <taiyesuleiman09@gmail.com>
2025-04-02 18:03:51 +0100

    implemented Authentication for users

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
 const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 module.exports = app
---
+++ models/userModel.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+  passwordChangedAt: Date,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+})
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next()
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined
+  next()
+})
+
+userSchema.pre('save', function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password') || this.isNew) return next()
+
+  // Set the passwordChangedAt property to current date + 1 second
+  this.passwordChangedAt = Date.now() - 1000
+  next()
+})
+
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } })
+  next()
+})
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
